a32db589f40d9780db0335b6e789ab06bed0ff50
Lina Teckentrup <46699837+lteckentrup@users.noreply.github.com>
2021-03-01 14:34:44 +1100

    Update NBP_CVeg_CSoil.py

--- analysis_python/NBP_CVeg_CSoil.py
+++ analysis_python/NBP_CVeg_CSoil.py
@@ -47,23 +47,37 @@ dataframes = [df_NBP, df_CVeg, df_CSoil]
 vars = ['nbp', 'cVeg', 'cSoil']
 
 for df, v in zip(dataframes, vars):
-    df['CABLE-POP'] = mask(v, total_mask, 'CABLE-POP')
-    df['CLASS-CTEM'] = mask(v, total_mask, 'CLASS-CTEM')
-    df['CLM5.0'] = mask(v, total_mask, 'CLM5.0')
-    df['ISAM'] = mask(v, total_mask, 'ISAM')
-    df['ISBA-CTRIP'] = mask(v, total_mask, 'ISBA-CTRIP')
-    df['JSBACH'] = mask(v, total_mask, 'JSBACH')
-    df['JULES-ES'] = mask(v, total_mask, 'JULES-ES')
-    df['LPX-Bern'] = mask(v, total_mask, 'LPX-Bern')
-    df['OCN'] = mask(v, total_mask, 'OCN')
-    df['ORCHIDEE'] = mask(v, total_mask, 'ORCHIDEE')
-    df['ORCHIDEE-CNP'] = mask(v, total_mask, 'ORCHIDEE-CNP')
-    df['SDGVM'] = mask(v, total_mask, 'SDGVM')
-    df['VISIT'] = mask(v, total_mask, 'VISIT')
+    df['CABLE-POP'] = mask('S3', v, total_mask, 'CABLE-POP')
+    df['CLASS-CTEM'] = mask('S3', v, total_mask, 'CLASS-CTEM')
+    df['CLM5.0'] = mask('S3', v, total_mask, 'CLM5.0')
+    df['ISAM'] = mask('S3', v, total_mask, 'ISAM')
+    df['ISBA-CTRIP'] = mask('S3', v, total_mask, 'ISBA-CTRIP')
+    df['JSBACH'] = mask('S3', v, total_mask, 'JSBACH')
+    df['JULES-ES'] = mask('S3', v, total_mask, 'JULES-ES')
+    df['LPX-Bern'] = mask('S3', v, total_mask, 'LPX-Bern')
+    df['OCN'] = mask('S3', v, total_mask, 'OCN')
+    df['ORCHIDEE'] = mask('S3', v, total_mask, 'ORCHIDEE')
+    df['ORCHIDEE-CNP'] = mask('S3', v, total_mask, 'ORCHIDEE-CNP')
+    df['SDGVM'] = mask('S3', v, total_mask, 'SDGVM')
+    df['VISIT'] = mask('S3', v, total_mask, 'VISIT')
 
 df_NBP['max'] = df_NBP.max(axis=1)
 df_NBP['min'] = df_NBP.min(axis=1)
+# df['diff'] = df['max']-df['min']
+# print(df['diff'].max(axis=0))
+# print(df['diff'].idxmax(axis=0))
+# #
+# print(df['diff'][72])
+# print(df['max'][72])
+# print(df['min'][72])
+# print(df['max'].max(axis=0))
+# print(df['min'].min(axis=0))
+# print(df['diff'].mean(axis=0))
+#
 df_NBP['mean'] = df_NBP.mean(axis=1)
+df_NBP['std'] = df_NBP.std(axis=1)
+df_NBP['mean+std'] = df_NBP['mean'] + df_NBP['std']
+df_NBP['mean-std'] = df_NBP['mean'] - df_NBP['std']
 
 df_NBP['year'] = np.arange(1901,2018)
 
@@ -73,6 +87,14 @@ ax1.plot(df_NBP['year'], df_NBP['mean'],lw=3.0, ls="-",
 ax1.fill_between(df_NBP['year'], df_NBP['min'], df_NBP['max'],
                  color =  'tab:green', alpha = 0.15, label = 'Model spread')
 
+ax1.set_ylim([-0.95,1.7])
+
+ax1.axhline(linewidth=2, color='k', alpha = 0.5)
+ax1.legend(loc='upper left', ncol=1, fancybox=False, frameon=False, fontsize=12)
+
+ax1.set_ylabel('NBP [PgC yr-1]')
+ax1.set_title('Annual NBP Australia')
+
 ## Cumulative sum individual models
 for mn, c in zip(model_names, colours):
     ax2.plot(df_NBP['year'], df_NBP[mn].cumsum(), color = c,
@@ -80,27 +102,22 @@ for mn, c in zip(model_names, colours):
     ax3.plot(df_NBP['year'],df_CVeg[mn], color = c, lw = 2.0, label = mn)
     ax4.plot(df_NBP['year'],df_CSoil[mn], color = c, lw = 2.0, label = mn)
 
-obs = mask('Aboveground Biomass Carbon', total_mask, mn)
+obs = mask('', 'Aboveground Biomass Carbon', total_mask, mn)
 ax3.plot(df_NBP['year'][-25:-6], obs, color = 'k', lw = 4.0, label = 'VOD')
 
-ax1.axhline(linewidth=2, color='k', alpha = 0.5)
 ax1.set_xticklabels([])
-ax1.set_ylim([-0.95,1.7])
-ax1.set_ylabel('NBP [PgC yr-1]')
-ax1.legend(loc='upper left', ncol=1, fancybox=False, frameon=False, fontsize=12)
-ax1.set_title('Annual NBP Australia')
 
 ax2.axhline(linewidth=1, color='k', alpha = 0.5)
-ax2.set_xticklabels([])
-ax2.set_ylabel('Cumulative NBP [PgC]')
 ax2.set_title('Cumulative NBP Australia')
+ax2.set_ylabel('Cumulative NBP [PgC]')
+ax2.set_xticklabels([])
 
-ax3.set_ylabel('$\mathrm{C_{Veg}}$ [PgC]')
 ax3.legend(loc='upper center', bbox_to_anchor=(1.1, -0.1), ncol=4)
 ax3.set_title('Carbon stored in vegetation Australia')
+ax3.set_ylabel('$\mathrm{C_{Veg}}$ [PgC]')
 
-ax4.set_ylabel('$\mathrm{C_{Soil}}$ [PgC]')
 ax4.set_title('Carbon stored in soil Australia')
+ax4.set_ylabel('$\mathrm{C_{Soil}}$ [PgC]')
 
 text(0.04, 1.02, 'a)', ha='center',transform=ax1.transAxes, fontsize = 14)
 text(0.04, 1.02, 'b)', ha='center',transform=ax2.transAxes, fontsize = 14)
